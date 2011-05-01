5b226dd6109fce75aa3533d74e0f221ee9011341
Luke L <dachande663@gmail.com>
2011-05-01 01:01:52 +0100

    Initial Commit

---
+++ PlexRemote/PlexRemoteAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  PlexRemoteAppDelegate.m
+//  PlexRemote
+//
+//  Created by Luke Lanchester on 01/05/2011.
+//  Copyright 2011 Luke Lanchester. All rights reserved.
+//
+
+#import "PlexRemoteAppDelegate.h"
+
+@implementation PlexRemoteAppDelegate
+
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
